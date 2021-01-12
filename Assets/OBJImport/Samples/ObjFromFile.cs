5a7e8efa3d2d2a9e73bde0fcaeea0782b5fa5163
Vivian Ross <vivianross06@users.noreply.github.com>
2021-01-11 19:11:42 -0800

    obj importer can load .obj file in matterport_mesh

---
+++ Assets/OBJImport/Samples/ObjFromFile.cs
@@ -0,0 +1,50 @@
+﻿using Dummiesman;
+using System.IO;
+using UnityEngine;
+
+public class ObjFromFile : MonoBehaviour
+{
+    public string house; //17DRP5sb8fy
+    public string objFile; //bed1a77d92d64f5cbbaaae4feed64ec1
+    string error = string.Empty;
+    GameObject loadedObject;
+
+    private void Start()
+    {
+        //make path match matterport irectory
+        string fullPath = Application.dataPath + "/../../matterport/" + house + "/matterport_mesh/" + objFile + "/" + objFile + ".obj";
+        if (loadedObject != null)
+            Destroy(loadedObject);
+        loadedObject = new OBJLoader().Load(fullPath);
+
+    }
+
+    /*
+    void OnGUI() {
+        //objPath = GUI.TextField(new Rect(0, 0, 256, 32), objPath);
+
+        //GUI.Label(new Rect(0, 0, 256, 32), "Obj Path:");
+        //if(GUI.Button(new Rect(256, 32, 64, 32), "Load File"))
+        //{
+        //file path
+        string fullPath = objPath + house + "/matterport_mesh/" + objFile + "/" + objFile + ".obj";
+            if (!File.Exists(fullPath))
+            {
+                error = "File doesn't exist.";
+            }else{
+                if(loadedObject != null)            
+                    Destroy(loadedObject);
+                loadedObject = new OBJLoader().Load(fullPath);
+                error = string.Empty;
+            }
+        //}
+
+        if(!string.IsNullOrWhiteSpace(error))
+        {
+            GUI.color = Color.red;
+            GUI.Box(new Rect(0, 64, 256 + 64, 32), error);
+            GUI.color = Color.white;
+        }
+    }
+    */
+}
---
+++ Assets/Scripts/cameraControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour
+{
+    private float speed = 15f;
+    private float sensitivity = 30f;
+    private float maxYAngle = 80f;
+    private Vector2 currentRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
+        currentRotation.y -= Input.GetAxis("Mouse Y") * sensitivity;
+        currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
+        currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle);
+        transform.rotation = Quaternion.Euler(currentRotation.y, currentRotation.x, 0);
+    }
+}
