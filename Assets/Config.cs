23182d464210799cc4febf01e50be5d24d0dc581
Vivian Ross <vivianross06@users.noreply.github.com>
2021-01-20 12:22:28 -0800

    added Config.cs file back

---
+++ Assets/Config.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MatterportUnity
+{
+    public class Config
+    {
+        public static string MATTERPORT_HOME = "/matterport/"; //TODO: Set path to matterport data
+    }
+}
