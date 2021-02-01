572553977ae04746498e08dbc43caf251b032eee
Vivian Ross <vivianross06@users.noreply.github.com>
2021-01-31 16:49:36 -0800

    updated config.cs to contain camp roberts path

--- Assets/Config.cs
+++ Assets/Config.cs
@@ -9,3 +9,11 @@ namespace MatterportUnity
         public static string MATTERPORT_HOME = "/matterport/"; //TODO: Set path to matterport data
     }
 }
+
+namespace CampRobertsUnity
+{
+    public class Config
+    {
+        public static string CR_HOME = "/camproberts/"; //TODO: set path to camp roberts data
+    }
+}
