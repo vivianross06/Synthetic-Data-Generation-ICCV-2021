01d8f3fc55bc744430b0f1859d42deb1a6668b01
Vivian Ross <vivianross06@users.noreply.github.com>
2021-04-19 11:50:18 -0700

    re add config.cs

---
+++ Assets/Config.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OmniLoaderUnity
+{
+    public class Config
+    {
+        public static string SUNCG_HOME = Application.dataPath + "/../../SUNCG/suncg/"; //TODO: set path to SUNCG 
+        public static string MATTERPORT_HOME = Application.dataPath + "/../../matterport/"; //TODO: Set path to matterport data
+        public static string CR_HOME = "D:/4Eyes/CampRobertsData/Camp_Roberts_CACTF_OBJ/Data/"; //TODO: set path to camp roberts data 
+    }
+
+}
+
+//namespace CampRobertsUnity
+//{
+//    public class Config
+//    {
+//        public static string CR_HOME = "/camproberts/"; //TODO: set path to camp roberts data
+//    }
+//}
